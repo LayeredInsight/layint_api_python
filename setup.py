cbd5263e11f7b52fc820d949243c8707d41e8966
Scott Oberg <Scott Oberg>
2018-10-16 13:12:49 -0700

    bump version to mark api_develop-20181015 build for pypi distribution

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "layint-api"
-VERSION = "0.10"
+VERSION = "0.10a1"
 # To install the library, run the following
 #
 # python setup.py install
